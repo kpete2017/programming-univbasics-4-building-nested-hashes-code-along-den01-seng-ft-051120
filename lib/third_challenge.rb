adc3ca6d5db072fb4f31779e137c021775ad9def
Kyle Petersen <kpete2017@gmail.com>
2020-04-16 21:08:26 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,7 @@ def third_challenge
         },
       :hero => {
         :name => "Romeo",
-        :age => "15"
+        :age => "15",
         :status => "alive"
         },
       :hero_friends => []
@@ -26,7 +26,9 @@ def third_challenge
         :age => "54"
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
